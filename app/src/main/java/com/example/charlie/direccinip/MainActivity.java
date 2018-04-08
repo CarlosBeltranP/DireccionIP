0ff4728ef5d5f115b7d8e5c7558ac25ab75cef39
CarlosBeltranP <00507511@uca.edu.sv>
2018-04-07 23:56:04 -0600

    Commit Final   - Ordenamiento de código (identación)   - Comentarios   - Margenes de diseño Material design

--- app/src/main/java/com/example/charlie/direccinip/MainActivity.java
+++ app/src/main/java/com/example/charlie/direccinip/MainActivity.java
@@ -9,7 +9,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    //declarando variables
     EditText octal1, octal2, octal3, octal4, m;
     int m1, m2, m3, m4, cantH, pRed, pHost;
     float x, y, r;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Button botonCalcular = findViewById(R.id.btnCalcular);
         ImageButton botonLimpiar = findViewById(R.id.btnLimpiar);
 
+        //Declarando nuestras View variables
         final TextView nId1 = (TextView) findViewById(R.id.NetID1);
         final TextView nId2 = (TextView) findViewById(R.id.NetID2);
         final TextView nId3 = (TextView) findViewById(R.id.NetID3);
@@ -58,47 +60,57 @@ public class MainActivity extends AppCompatActivity {
                 int o3 = Integer.valueOf(octal3.getText().toString());
                 int o4 = Integer.valueOf(octal4.getText().toString());
 
+                //calcula parte entera (octetos 255) y residuo (para calcular el valor de la mascara del ultimo octeto)
                 x = mascara / 8;
                 y = mascara % 8;
                 r = 8 - y;
 
-                if(x==0){
-                    m1 = (int) (256 - (Math.pow(2, (8-y))));
-                    m2 = 0;
+                if(x==0){ //cuando el valor de la mascara sea 8 o menos
+                    m1 = (int) (256 - (Math.pow(2, (8-y)))); //calculo el valor del primer octeto de la mascara
+                    m2 = 0; //los demás octetos serán cero
                     m3 = 0;
                     m4 = 0;
 
+                    //ingreso a las variables mask el valor calculado de los octetos de la mascara
                     mask1.setText(""+m1);
                     mask2.setText(""+m2);
                     mask3.setText(""+m3);
                     mask4.setText(""+m4);
 
+                    //CALCULO DE NETID
+                    /*realizo la operacion binaria AND a nivel de bits entre los valores de cada octeto de la mascara
+                    con los valores de cada octeto de la direccion IP ingresada*/
                     int n1 = o1 & m1;
                     int n2 = o2 & m2;
                     int n3 = o3 & m3;
                     int n4 = o4 & m4;
-
+                    //muestro en los TextView los resultados de la NetId
                     nId1.setText(""+n1);
                     nId2.setText(""+n2);
                     nId3.setText(""+n3);
                     nId4.setText(""+n4);
 
+                    //CALCULO DE BROADCASTID
                     int b1 = (int) (n1 + (Math.pow(2, r) -1));
                     int b2 = 255;
                     int b3 = 255;
                     int b4 = 255;
-
+                    //muestro en los TextView los resultados de la BroadcastID
                     bd1.setText(""+b1);
                     bd2.setText(""+b2);
                     bd3.setText(""+b3);
                     bd4.setText(""+b4);
 
+                    //CALCULO DE cantidad de Host en la red
                     cantH = (int) ((Math.pow(2, (32-mascara)))-2);
+                    //muestro en el Txtview el resultado de la cantidad de Host
                     cantHost.setText(""+cantH);
 
+                    //CALCULO DE PARTE DE RED
                     pRed = (int) Math.pow(2, mascara);
                     parteRed.setText(""+pRed);
 
+                    //CALCULO DE PARTE DE HOST
                     pHost = (int) Math.pow(2, (32 - mascara));
                     parteHost.setText(""+pHost);
 
@@ -276,6 +288,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //boton limpiar campos
         botonLimpiar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
